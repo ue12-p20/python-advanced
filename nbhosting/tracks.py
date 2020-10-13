a4808d9542295a0501a2d61999d3369c0dd69cd4
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-10-13 16:31:44 +0200

    provision for all quizzes

--- nbhosting/tracks.py
+++ nbhosting/tracks.py
@@ -17,13 +17,13 @@ def tracks(coursedir):
         ('Python avancé', 'Python avancé', 'python', [
             ('1/9: jeux', 'notebooks/1-*.md', 'tps/games/README.md'),
             ('2/9: types de base', 'quiz/2*.md', 'notebooks/2-*.md', 'tps/dijkstra/README.md'),
-            ('3/9: itérations', 'notebooks/3-*.md'),
-            ('4/9: fonctions', 'notebooks/4-*.md'),
-            ('5/9: ???', 'notebooks/5-*.md'),
-            ('6/9: ???', 'notebooks/6-*.md'),
-            ('7/9: ???', 'notebooks/7-*.md'),
-            ('8/9: ???', 'notebooks/8-*.md'),
-            ('9/9: ???', 'notebooks/9-*.md'),
+            ('3/9: itérations', 'quiz/3*.md', 'notebooks/3-*.md'),
+            ('4/9: fonctions', 'quiz/4*.md', 'notebooks/4-*.md'),
+            ('5/9: ???', 'quiz/5*.md', 'notebooks/5-*.md'),
+            ('6/9: ???', 'quiz/6*.md', 'notebooks/6-*.md'),
+            ('7/9: ???', 'quiz/7*.md', 'notebooks/7-*.md'),
+            ('8/9: ???', 'quiz/8*.md', 'notebooks/8-*.md'),
+            ('9/9: ???', 'quiz/9*.md', 'notebooks/9-*.md'),
         ])]
 
     return [Track(coursedir,
